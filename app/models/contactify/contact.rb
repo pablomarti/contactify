bec5e5f61c885d11d710990158d8ae5c714a6601
Pablo Marti <pablo@greenmediaagency.com>
2013-08-22 15:27:40 -0600

    Add additional fields

--- app/models/contactify/contact.rb
+++ app/models/contactify/contact.rb
@@ -1,6 +1,7 @@
 module Contactify
   class Contact < ActiveRecord::Base
     attr_accessible :comment, :email, :first_name, :last_name
+    attr_accessible :phone, :country, :city
 
     validates :first_name, :last_name, :email, :presence => true
   end
