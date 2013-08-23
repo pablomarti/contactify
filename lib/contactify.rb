b801e8f2e22c3344799d3024032e8d097a81d809
Pablo Marti <pablo@greenmediaagency.com>
2013-08-23 09:34:57 -0600

    Fix path for concerns

--- lib/contactify.rb
+++ lib/contactify.rb
@@ -1,5 +1,5 @@
 require "contactify/engine"
-Dir[File.dirname(__FILE__) + "/billify/concerns/models/*.rb"].each{ |f| require "contactify/concerns/models/#{File.basename(f, ".rb")}" }
+Dir[File.dirname(__FILE__) + "/contactify/concerns/models/*.rb"].each{ |f| require "contactify/concerns/models/#{File.basename(f, ".rb")}" }
 
 module Contactify
 end
