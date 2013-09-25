f64ff795341c216f51a919b362e492a3a6fbd1fd
Pablo Marti <pablo@greenmediaagency.com>
2013-09-25 10:52:03 -0600

    Concerns for contacts controller

--- lib/contactify.rb
+++ lib/contactify.rb
@@ -1,5 +1,6 @@
 require "contactify/engine"
 Dir[File.dirname(__FILE__) + "/contactify/concerns/models/*.rb"].each{ |f| require "contactify/concerns/models/#{File.basename(f, ".rb")}" }
+Dir[File.dirname(__FILE__) + "/contactify/concerns/controllers/*.rb"].each{ |f| require "contactify/concerns/controllers/#{File.basename(f, ".rb")}" }
 
 module Contactify
 end
